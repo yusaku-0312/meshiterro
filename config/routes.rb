b6b7be44c42091c85e309be9f9bcf40f06312aa3
yusaku-0312 <161700710+yusaku-0312@users.noreply.github.com>
2024-09-18 14:04:07 +0000

    [Finish]4-13 投稿機能の作成(6)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :post_images, only: [:new, :create, :index, :show]
+  get 'post_image/index'
+  get 'post_image/show'
+  get 'post_image/new'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
